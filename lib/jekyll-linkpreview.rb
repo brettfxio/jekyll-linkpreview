81ed9e858c7689048ab99a313584c24eb04ecd6e
brettfxio <105930054+brettfxio@users.noreply.github.com>
2024-08-21 17:11:24 +0000

    Trying different User-Agent to avoid Cloudflare request problems.

--- lib/jekyll-linkpreview.rb
+++ lib/jekyll-linkpreview.rb
@@ -161,7 +161,9 @@ module Jekyll
 
       private
       def fetch(url)
-        MetaInspector.new(url)
+        MetaInspector.new(url, :headers => {
+          'User-Agent' => 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36'
+        })
       end
 
       private
